afd0adb16beee6af7096f402c9be9820f023597a
Juan David Chica <jdchica1992@gmail.com>
2018-04-28 21:26:23 -0500

    Se realiza los ajustes para las notificaciones PUSH y peticiones HTTP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Push, PushObject, PushOptions} from '@ionic-native/push';
 
-import { HomePage } from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
+
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+    rootPage: any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private  push:Push) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            statusBar.styleDefault();
+            splashScreen.hide();
+            this.PushSetup();
+        });
+    }
+
+    PushSetup(){
+        const options: PushOptions = {
+            android: {
+                senderID:'826068195608'
+            },
+            ios: {
+                alert: 'true',
+                badge: 'true',
+                sound: 'false'
+            }
+        };
+
+        const pushObject:PushObject =  this.push.init(options);
+        pushObject.on('notification').subscribe( (notification: any) => console.log('Recive a Notification', notification) );
+        pushObject.on('registration').subscribe( (registration: any) => console.log('Device Registered', registration) );
+        pushObject.on('error').subscribe( (error: any) => console.log('Error with Push Plugin', error) );
+    }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component, NgZone} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {HTTP} from '@ionic-native/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    dataurl: any = [];
+    data: any = [];
+    header: any = {};
 
-  }
+    constructor(public navCtrl: NavController, public http: HTTP, public zone: NgZone) {
+    }
 
+    ionViewDidEnter(){
+        this.header['Cache-Control'] = 'no-cache';
+        this.http.clearCookies();
+        this.http.get('http://172.20.10.6/',{},this.header)
+            .then(res =>{
+                this.zone.run(()=>{
+                   this.dataurl=JSON.parse(res.data);
+                   this.data = this.dataurl;
+                });
+            }).catch(e =>{
+                console.log(e);
+        });
+    }
+
+    getItems(ev) {
+        // Reset items back to all of the items
+        this.data = this.dataurl;
+
+        // set val to the value of the ev target
+        var val = ev.target.value;
+
+        // if the value is an empty string don't filter the items
+        if (val && val.trim() != '') {
+            this.data = this.data.filter((item) => {
+                return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            })
+        }
+    }
+
+    viewData(){
+
+    }
 }
